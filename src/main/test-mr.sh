fd4ab6d53f69233e7cdd5508c57198d55f527d67
zw <1018512223@qq.com>
2023-04-13 15:47:29 +0800

    commit base func complete

--- src/main/test-mr.sh
+++ src/main/test-mr.sh
@@ -176,27 +176,27 @@ wait
 #
 #
 ##########################################################
-#echo '***' Starting reduce parallelism test.
-#
-#rm -f mr-*
-#
-#maybe_quiet $TIMEOUT ../mrcoordinator ../pg*txt &
-#sleep 1
-#
-#maybe_quiet $TIMEOUT ../mrworker ../../mrapps/rtiming.so  &
-#maybe_quiet $TIMEOUT ../mrworker ../../mrapps/rtiming.so
-#
-#NT=`cat mr-out* | grep '^[a-z] 2' | wc -l | sed 's/ //g'`
-#if [ "$NT" -lt "2" ]
-#then
-#  echo '---' too few parallel reduces.
-#  echo '---' reduce parallelism test: FAIL
-#  failed_any=1
-#else
-#  echo '---' reduce parallelism test: PASS
-#fi
-#
-#wait
+echo '***' Starting reduce parallelism test.
+
+rm -f mr-*
+
+maybe_quiet $TIMEOUT ../mrcoordinator ../pg*txt &
+sleep 1
+
+maybe_quiet $TIMEOUT ../mrworker ../../mrapps/rtiming.so  &
+maybe_quiet $TIMEOUT ../mrworker ../../mrapps/rtiming.so
+
+NT=`cat mr-out* | grep '^[a-z] 2' | wc -l | sed 's/ //g'`
+if [ "$NT" -lt "2" ]
+then
+  echo '---' too few parallel reduces.
+  echo '---' reduce parallelism test: FAIL
+  failed_any=1
+else
+  echo '---' reduce parallelism test: PASS
+fi
+
+wait
 #
 ##########################################################
 #echo '***' Starting job count test.
