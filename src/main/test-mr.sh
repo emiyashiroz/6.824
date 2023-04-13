4e17a61c5436e3534e77901cfee11c0c707120c0
zw <1018512223@qq.com>
2023-04-13 16:50:30 +0800

    commit base func complete

--- src/main/test-mr.sh
+++ src/main/test-mr.sh
@@ -279,61 +279,61 @@ else
   echo '---' early exit test: FAIL
   failed_any=1
 fi
-#rm -f mr-*
-#
-##########################################################
-#echo '***' Starting crash test.
-#
-## generate the correct output
-#../mrsequential ../../mrapps/nocrash.so ../pg*txt || exit 1
-#sort mr-out-0 > mr-correct-crash.txt
-#rm -f mr-out*
-#
-#rm -f mr-done
-#((maybe_quiet $TIMEOUT2 ../mrcoordinator ../pg*txt); touch mr-done ) &
-#sleep 1
-#
-## start multiple workers
-#maybe_quiet $TIMEOUT2 ../mrworker ../../mrapps/crash.so &
-#
-## mimic rpc.go's coordinatorSock()
-#SOCKNAME=/var/tmp/5840-mr-`id -u`
-#
-#( while [ -e $SOCKNAME -a ! -f mr-done ]
-#  do
-#    maybe_quiet $TIMEOUT2 ../mrworker ../../mrapps/crash.so
-#    sleep 1
-#  done ) &
-#
-#( while [ -e $SOCKNAME -a ! -f mr-done ]
-#  do
-#    maybe_quiet $TIMEOUT2 ../mrworker ../../mrapps/crash.so
-#    sleep 1
-#  done ) &
-#
-#while [ -e $SOCKNAME -a ! -f mr-done ]
-#do
-#  maybe_quiet $TIMEOUT2 ../mrworker ../../mrapps/crash.so
-#  sleep 1
-#done
-#
-#wait
-#
-#rm $SOCKNAME
-#sort mr-out* | grep . > mr-crash-all
-#if cmp mr-crash-all mr-correct-crash.txt
-#then
-#  echo '---' crash test: PASS
-#else
-#  echo '---' crash output is not the same as mr-correct-crash.txt
-#  echo '---' crash test: FAIL
-#  failed_any=1
-#fi
-#
-##########################################################
-#if [ $failed_any -eq 0 ]; then
-#    echo '***' PASSED ALL TESTS
-#else
-#    echo '***' FAILED SOME TESTS
-#    exit 1
-#fi
+rm -f mr-*
+
+#########################################################
+echo '***' Starting crash test.
+
+# generate the correct output
+../mrsequential ../../mrapps/nocrash.so ../pg*txt || exit 1
+sort mr-out-0 > mr-correct-crash.txt
+rm -f mr-out*
+
+rm -f mr-done
+((maybe_quiet $TIMEOUT2 ../mrcoordinator ../pg*txt); touch mr-done ) &
+sleep 1
+
+# start multiple workers
+maybe_quiet $TIMEOUT2 ../mrworker ../../mrapps/crash.so &
+
+# mimic rpc.go's coordinatorSock()
+SOCKNAME=/var/tmp/5840-mr-`id -u`
+
+( while [ -e $SOCKNAME -a ! -f mr-done ]
+  do
+    maybe_quiet $TIMEOUT2 ../mrworker ../../mrapps/crash.so
+    sleep 1
+  done ) &
+
+( while [ -e $SOCKNAME -a ! -f mr-done ]
+  do
+    maybe_quiet $TIMEOUT2 ../mrworker ../../mrapps/crash.so
+    sleep 1
+  done ) &
+
+while [ -e $SOCKNAME -a ! -f mr-done ]
+do
+  maybe_quiet $TIMEOUT2 ../mrworker ../../mrapps/crash.so
+  sleep 1
+done
+
+wait
+
+rm $SOCKNAME
+sort mr-out* | grep . > mr-crash-all
+if cmp mr-crash-all mr-correct-crash.txt
+then
+  echo '---' crash test: PASS
+else
+  echo '---' crash output is not the same as mr-correct-crash.txt
+  echo '---' crash test: FAIL
+  failed_any=1
+fi
+
+#########################################################
+if [ $failed_any -eq 0 ]; then
+    echo '***' PASSED ALL TESTS
+else
+    echo '***' FAILED SOME TESTS
+    exit 1
+fi
